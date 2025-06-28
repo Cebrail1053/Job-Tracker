0db89f6d8feb657f3c62850ac24d11c7b6a27f32
Gabriel <gabriel.avila.ga09@gmail.com>
2025-06-27 21:24:07 -0500

    Updated DB

--- DB/jobtrackerdb.sql
+++ DB/jobtrackerdb.sql
@@ -5,9 +5,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema jobtrackerdb
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `jobtrackerdb` ;
@@ -15,21 +12,23 @@ DROP SCHEMA IF EXISTS `jobtrackerdb` ;
 -- -----------------------------------------------------
 -- Schema jobtrackerdb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `jobtrackerdb` ;
+CREATE SCHEMA IF NOT EXISTS `jobtrackerdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `jobtrackerdb` ;
 
 -- -----------------------------------------------------
 -- Table `jobtrackerdb`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `jobtrackerdb`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` BINARY(16) NOT NULL,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(75) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -39,21 +38,21 @@ CREATE TABLE IF NOT EXISTS `jobtrackerdb`.`application` (
   `job_id` INT NOT NULL,
   `job_title` VARCHAR(100) NOT NULL,
   `company` VARCHAR(100) NOT NULL,
-  `date_applied` DATETIME NULL,
-  `location` VARCHAR(60) NULL,
-  `portal_url` VARCHAR(2048) NULL,
-  `status` TINYINT(3) NOT NULL,
-  `notes` MEDIUMTEXT NULL,
-  `user_id` INT NOT NULL,
+  `date_applied` DATETIME NULL DEFAULT NULL,
+  `location` VARCHAR(60) NULL DEFAULT NULL,
+  `portal_url` VARCHAR(2048) NULL DEFAULT NULL,
+  `status` TINYINT NOT NULL,
+  `notes` MEDIUMTEXT NULL DEFAULT NULL,
+  `user_id` BINARY(16) NOT NULL,
   PRIMARY KEY (`job_id`, `user_id`),
   UNIQUE INDEX `job_id_UNIQUE` (`job_id` ASC) VISIBLE,
   INDEX `fk_application_user_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_application_user`
     FOREIGN KEY (`user_id`)
-    REFERENCES `jobtrackerdb`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `jobtrackerdb`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
